8b36d4d37e2c9e032e9e70a1fb06b8df841e686c
Apprentice <apprentice@devbootcamp.com>
2013-06-26 11:45:18 -0700

    Changed linking on urls printed out.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,9 +8,7 @@ post '/urls' do
   redirect '/'
 end
 
-# e.g., /q6bda
 get '/:short_url' do
   Url.find_by_short_url(params[:short_url]).clicked!
-  puts params
   redirect Url.find_by_short_url(params[:short_url]).full_url
 end
