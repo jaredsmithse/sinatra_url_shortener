bf0adc22c1a7d6a643c1e5783fb8e1e77fd46487
Jared Smith <jared.rit@me.com>
2013-06-26 20:52:06 -0700

    Getting for heroku

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,7 +8,7 @@ post '/urls' do
   redirect '/'
 end
 
-get '/:short_url' do
+get '/url/:short_url' do
   Url.find_by_short_url(params[:short_url]).clicked!
   redirect Url.find_by_short_url(params[:short_url]).full_url
 end
